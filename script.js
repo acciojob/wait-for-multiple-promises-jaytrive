ef8ee0e4b6d72517eb14203863db86df47ece6ab
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-03-29 17:09:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const promise1 = new Promise((resolve,reject) => {
+/*const promise1 = new Promise((resolve,reject) => {
 	const time=Math.ceil(Math.random()*3);
 	setTimeout(() => {
 		resolve(time);
@@ -28,4 +28,36 @@ Promise.all(promises).then((res) => {
 	const totalTime=res.reduce((val,cur) => val+cur,0);
 	res.map((val,index) => tbody.innerHTML+= `<td>Promise ${index+1}</td><td>${val}</td>`)
 	tbody.innerHTML+=`<td>Total</td><td>${totalTime.toFixed(3)}</td>`
-})
+})*/
+
+const promises = [
+    new Promise(resolve => setTimeout(() => resolve(Math.random() * 2 + 1), Math.random() * 2000 + 1000)),
+    new Promise(resolve => setTimeout(() => resolve(Math.random() * 2 + 1), Math.random() * 2000 + 1000)),
+    new Promise(resolve => setTimeout(() => resolve(Math.random() * 2 + 1), Math.random() * 2000 + 1000))
+  ];
+
+  Promise.all(promises)
+    .then(results => {
+      const loadingRow = document.getElementById("loading");
+      loadingRow.parentNode.removeChild(loadingRow);
+
+      const table = document.querySelector("table");
+      let total = 0;
+
+      results.forEach((result, index) => {
+        const promiseNum = index + 1;
+        const timeTaken = result.toFixed(3);
+        total += result;
+        const row = table.insertRow();
+        const promiseCell = row.insertCell(0);
+        const timeTakenCell = row.insertCell(1);
+        promiseCell.textContent = `Promise ${promiseNum}`;
+        timeTakenCell.textContent = timeTaken;
+      });
+
+      const totalRow = table.insertRow();
+      const totalCell = totalRow.insertCell(0);
+      const totalTimeCell = totalRow.insertCell(1);
+      totalCell.textContent = "Total";
+      totalTimeCell.textContent = total.toFixed(3);
+    });
