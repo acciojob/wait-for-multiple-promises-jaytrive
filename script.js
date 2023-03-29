ccf13b72fdcb95361847eb18a9e805aeddb42189
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-03-29 16:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const promise1 = new Promise((resolve,reject) => {
+	const time=Math.ceil(Math.random()*3);
+	setTimeout(() => {
+		resolve(time);
+	},time*1000)
+})
+
+const promise2 = new Promise((resolve,reject) => {
+	const time=Math.ceil(Math.random()*3);
+	setTimeout(() => {
+		resolve(time);
+	},time*1000)
+})
+
+const promise3 = new Promise((resolve,reject) => {
+	const time=Math.ceil(Math.random()*3);
+	setTimeout(() => {
+		resolve(time);
+	},time*1000)
+})
+const tbody=document.getElementById('output');
+const promises=[promise1,promise2,promise3];
+
+Promise.all(promises).then((res) => {
+	const totalTime=res.reduce((val,cur) => val+cur,0);
+	res.map((val,index) => tbody.innerHTML+= `<td>Promise ${index+1}</td><td>${val}</td>`)
+	tbody.innerHTML+=`<td>Total</td><td>${totalTime.toFixed(3)}</td>`
+})
